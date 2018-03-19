807c573c76650057b3ab357ff9d7aa36e8899845
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-03-19 15:42:47 +0530

    Update and rename 95.c to 95.cpp

---
+++ 95.cpp
@@ -0,0 +1,10 @@
+#include"iostream"
+using namespace std;
+int main()
+{
+  int p,t,r,si;
+  cout<<" Enter principal amount time and rate : ";
+  cin>>p>>t>>r;
+  si=(p*t*r)/100;
+  cout<<" Value : "<<si;
+}
